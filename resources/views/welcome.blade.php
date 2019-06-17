a423396f6eeb321f3b5023d88dad52392e7a3a27
techgitek <techgitek@gmail.com>
2019-06-17 02:40:03 +0000

    init Laravel Project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    taro
                 </div>
 
                 <div class="links">

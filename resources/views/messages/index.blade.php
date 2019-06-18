3470614cfa9141c9f9aefb216ee45fd1a953f3b0
techgitek <techgitek@gmail.com>
2019-06-18 07:26:29 +0000

    CRUD messages

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -15,12 +15,14 @@
             <tbody>
                 @foreach ($messages as $message)
                 <tr>
-                    <td>{{ $message->id }}</td>
+                    <td>{!! link_to_route('messages.show', $message->id, ['id' => $message->id]) !!}</td>
                     <td>{{ $message->content }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
+    
+    {!! link_to_route('messages.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
